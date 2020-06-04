7d38e4e8c7d92de71c5a83b8163d02ea1ec73d93
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-06-04 05:57:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ function breakOut(array, changeValue, stopValue) {
 }
 
 function keepGoing(array, changeValue, skipValue) {
-  
+  for(let i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue;
+    }
+  }
 }
