06bd96074bc91a0e02c53db04598d11b48a91d7d
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-06-04 06:07:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for(let i = 0; i < array.length; i++) {
-    if (findfn(array[i])) {
+    if (findFn(array[i])) {
       return array[i]
     }
     return null
